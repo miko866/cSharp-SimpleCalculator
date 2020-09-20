0b03032bb76459a031d7f9e5e34e589519234fc0
Michal Durik <michal.durik@evosys.ch>
2020-09-20 20:40:37 +0200

    first commit

---
+++ SimpleCalturator.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,32 @@
+using System;
+using Xunit;
+
+namespace SimpleCalturator.Test.Unit
+{
+    public class CalculatorEngineTest
+    {
+        private readonly SimpleCalculator.CalculatorEngine _calculatorEngine = new SimpleCalculator.CalculatorEngine();
+
+        [Fact]
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("add", number1, number2);
+
+            Assert.Equal(3, result);
+
+        }
+
+        [Fact]
+        public void AddsTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("+", number1, number2);
+
+            Assert.Equal(3, result);
+
+        }
+    }
+}
